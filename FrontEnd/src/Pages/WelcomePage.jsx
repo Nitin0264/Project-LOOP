8f500768239855afd0caee0364d54db8c9ad0414
Nitin0264 <nc8224140@gmail.com>
2026-08-07 20:37:55 +0530

    checking tailwind and downloaded the packeges

---
+++ FrontEnd/src/Pages/WelcomePage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function WelcomePage() {
+  return (
+    <div className='flex items-center justify-center gap-2'>
+      <h1>PROJECT LOOP</h1>
+      <h3 className='m-4'>AI-Powered Customer Feedback Intelligence Platform
+
+        Collect, organize and analyze customer feedback
+        using Artificial Intelligence. </h3>
+
+      <p>get strted link </p>
+      <p>learn more link</p>
+    </div>
+  )
+}
+
+export default WelcomePage
